efd3c4ac1118cab2a812e6bd48bfba96df73893f
CoderMahabub <codermahabub@gmail.com>
2021-08-06 11:20:02 +0600

    Math. function

---
+++ math.js
@@ -0,0 +1,31 @@
+
+//Absolute Value
+// const myNumber = -5;
+// const output = Math.abs(myNumber);
+// console.log(output);
+
+//Ceil Number(Round Figure with Next Number)
+// const myNumber = 21.000323;
+// const output = Math.ceil(myNumber);
+// console.log(output);
+
+//Floor Number(Round Figure with Previous Number)
+// const myNumber = 21.000323;
+// const output = Math.floor(myNumber);
+// console.log(output);
+
+
+//Round Number (Nearest Round Number)
+const myNumber = 21.4323;
+// const output = Math.round(myNumber);
+// console.log(output);
+
+//Lodu Chokka
+for (let i = 0; i <= 20; i++) {
+    const myNumber = 21.4323;
+    const output = Math.random(myNumber) * 7;
+    const rounded = Math.floor(output);
+    console.log(rounded);
+}
+
+
