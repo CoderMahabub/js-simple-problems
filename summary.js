b66e769c1883998c65cb8d7130db0e0311901296
CoderMahabub <codermahabub@gmail.com>
2021-08-08 01:26:38 +0600

    Recursion Problem Doesn't solve

---
+++ summary.js
@@ -0,0 +1,38 @@
+//Math.floor
+//Math.ceil
+//Math.round
+
+// const number = 1.2356;
+// console.log(Math.round(number));
+
+
+//Math.roun with Math.random
+// const random = Math.random();
+// const between10 = random * 10;
+// const rounded = Math.round(between10);
+// console.log(rounded);
+// const selected = [];
+// for (let i = 0; i < 5; i++) {
+//     const random = Math.random() * 100;
+//     const picked = Math.round(random);
+//     console.log(selected);
+//     if (selected.indexOf(picked) == -1) {
+//         selected.push(picked);
+//     } else {
+//         console.log(selected, picked);
+//     }
+// }
+// console.log(selected);
+
+
+
+
+function add(a, b) {
+    return a + b;
+}
+let addvalue = add(12, 12);
+console.log(addvalue);
+
+
+
+
